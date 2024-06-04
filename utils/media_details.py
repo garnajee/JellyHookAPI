e512bad67bb39c20d500dad817c3c503578e5d25
Garnajee <>
2024-06-04 22:14:38 +0200

    feat: Add utility functions for media details
    - Implemented get_tmdb_details to fetch media details from TMDB API
    - Added imdb_to_tmdb function to convert IMDb IDs to TMDB IDs
    - Implemented get_trailer_link and search_trailer_key to fetch trailer links from TMDB API
    - Added is_season_ep_or_movie to determine media type based on title

---
+++ utils/media_details.py
@@ -0,0 +1,148 @@
+#!/usr/bin/env python3
+import requests
+import re
+import os
+from dotenv import load_dotenv
+
+# Charger les variables d'environnement globales
+load_dotenv()
+
+TMDB_API_KEY = os.getenv("TMDB_API_KEY")
+LANGUAGE = os.getenv("LANGUAGE")
+LANGUAGE2 = os.getenv("LANGUAGE2")
+BASE_URL = "https://api.themoviedb.org/3"
+
+def get_tmdb_details(media_type: str, tmdbid: int, language: str = LANGUAGE) -> dict:
+    """
+    Get details from TMDB API.
+
+    Args:
+        media_type (str): Type of media (movie, tv).
+        tmdbid (int): TMDB ID of the media.
+        language (str, optional): Language for the details. Defaults to LANGUAGE.
+
+    Returns:
+        dict: Details of the media.
+    """
+    url = f"{BASE_URL}/{media_type}/{tmdbid}"
+    params = {
+        'api_key': TMDB_API_KEY,
+        'language': language,
+    }
+    response = requests.get(url, params=params, timeout=10)
+    return response.json()
+
+def imdb_to_tmdb(imdb_id: str) -> str:
+    """
+    Get TMDB ID from IMDb ID.
+
+    Args:
+        imdb_id (str): IMDb ID of the media.
+
+    Returns:
+        str: TMDB link.
+    """
+    url = f"{BASE_URL}/find/{imdb_id}?external_source=imdb_id&language={LANGUAGE}&api_key={TMDB_API_KEY}"
+    try:
+        response = requests.get(url, timeout=10)
+        response.raise_for_status()
+        results = response.json()
+        if "movie_results" in results and results["movie_results"]:
+            return f"https://tmdb.org/movie/{results['movie_results'][0]['id']}"
+        elif "tv_results" in results and results["tv_results"]:
+            return f"https://tmdb.org/tv/{results['tv_results'][0]['id']}"
+        elif "tv_episode_results" in results and results["tv_episode_results"]:
+            return f"https://tmdb.org/tv/episode/{results['tv_episode_results'][0]['id']}"
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
+    return None
+
+def get_trailer_link(media_type: str, tmdbid: int) -> str:
+    """
+    Get the Youtube trailer link.
+
+    Args:
+        media_type (str): Type of media (movie, tv).
+        tmdbid (int): TMDB ID of the media.
+
+    Returns:
+        str: Trailer link.
+    """
+    languages = [(LANGUAGE, r"bande[-\s]?annonce"), (LANGUAGE2, r"trailer")]
+    trailer_links = []
+    vidt = f"{media_type}/{tmdbid}"
+    for language, pattern in languages:
+        youtube_key = search_trailer_key(vidt, language, pattern)
+        if youtube_key:
+            trailer_links.append(f"https://youtu.be/{youtube_key}")
+    if trailer_links:
+        if len(trailer_links) == 2:
+            return f"• Trailer FR: {trailer_links[0]}\n • Trailer EN: {trailer_links[1]}"
+        elif len(trailer_links) == 1:
+            return f"• Trailer: {trailer_links[0]}\n"
+    return ""
+
+def search_trailer_key(vidt: str, language: str, pattern: str) -> str:
+    """
+    Search for the trailer key in the TMDB API.
+
+    Args:
+        vidt (str): Video type and ID.
+        language (str): Language for the search.
+        pattern (str): Regex pattern to search for.
+
+    Returns:
+        str: Trailer key if found, None otherwise.
+    """
+    regex = re.compile(r"^[a-z]+/[0-9]+")
+    if "season" in vidt:
+        vidt = regex.findall(vidt)[0]
+    url = f"{BASE_URL}/{vidt}/videos"
+    params = {
+        'api_key': TMDB_API_KEY,
+        'language': language,
+    }
+    try:
+        response = requests.get(url, params=params, timeout=10)
+        if response.status_code == 200:
+            data = response.json()
+            results = data.get("results", [])
+            for video in results:
+                if re.search(pattern, video.get("name", ""), flags=re.IGNORECASE):
+                    return video.get("key")
+    except requests.exceptions.RequestException as e:
+        print(f"search_trailer_key request error: {e}")
+    return None
+
+def is_season_ep_or_movie(media_type: str, title: str) -> str:
+    """
+    Check if it's a season, episode or a movie.
+
+    Args:
+        media_type (str): Type of media (movie, tv).
+        title (str): Title of the media.
+
+    Returns:
+        str: "season", "episode" or "movie".
+    """
+    if media_type == "movie":
+        return "movie"
+    elif media_type == "tv":
+        if re.search(r"Season-added\s*", title, flags=re.IGNORECASE):
+            return "season"
+        elif re.search(r"Episode-added\s*", title, flags=re.IGNORECASE):
+            return "episode"
+        else:
+            return "serie"
+    return None
+
+if __name__ == "__main__":
+    media_type = "movie"
+    tmdbid = "550"
+    print(get_tmdb_details(media_type, tmdbid))
+    imdb_id = "tt0137523"
+    print(imdb_to_tmdb(imdb_id))
+    print(get_trailer_link(media_type, tmdbid))
+    title = "Season-added: The Mandalorian, Saison 2"
+    print(is_season_ep_or_movie("tv", title))
+
