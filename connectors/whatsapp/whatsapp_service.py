15c562818fa716bb53e3408d2055b8a82a81d57b
Garnajee <>
2024-06-07 17:37:23 +0200

    feat: format message before sending message

--- connectors/whatsapp/whatsapp_service.py
+++ connectors/whatsapp/whatsapp_service.py
@@ -12,13 +12,32 @@ WHATSAPP_NUMBER = os.getenv("WHATSAPP_NUMBER")
 WHATSAPP_API_USERNAME = os.getenv("WHATSAPP_API_USERNAME")
 WHATSAPP_API_PWD = os.getenv("WHATSAPP_API_PWD")
 
-def send_message(message: str, options: dict = {}) -> requests.Response:
+def format_message(message: dict) -> str:
+    """
+    Formater le message pour WhatsApp.
+
+    Args:
+        message (dict): Le message à envoyer.
+
+    Returns:
+        str: Le message formaté pour WhatsApp.
+    """
+    formatted_message = f"*{message.get('title')}*\n"
+    if message.get('description'):
+        formatted_message += f"```{message.get('description')}```\n"
+    if message.get('media_link'):
+        formatted_message += f"{message.get('media_link')}\n"
+    if message.get('trailer'):
+        formatted_message += message.get('trailer')
+    return formatted_message
+
+def send_message(message: dict, options: dict = None) -> requests.Response:
     """
     Send a message to WhatsApp API.
 
     Args:
-        message (str): Message to be sent.
-        options (dict): Options specific to the API
+        message (dict): Message to be sent.
+        options (dict, optional): Options specific to the API
 
     Returns:
         requests.Response: Response from the WhatsApp API.
@@ -26,33 +45,38 @@ def send_message(message: str, options: dict = {}) -> requests.Response:
     send_image = options.get('send_image', False)
     picture_path = options.get('picture_path', None)
 
-    url = f"{WHATSAPP_API_URL}/send/image" if send_image else f"{WHATSAPP_API_URL}/send/message"
+    url = f"{WHATSAPP_API_URL}/send/image" if options and send_image else f"{WHATSAPP_API_URL}/send/message"
     auth = (WHATSAPP_API_USERNAME, WHATSAPP_API_PWD)
     headers = {'accept': 'application/json'}
+
+    formatted_message = format_message(message)
     data = {'phone': WHATSAPP_NUMBER}
 
-    try:
-        if send_image and picture_path:
-            data['caption'] = message
-            data['compress'] = "True"
-            files = {'image': ('image', open(picture_path, 'rb'), 'image/png')}
-            response = requests.post(url, headers=headers, data=data, auth=auth, files=files)
-        else:
-            data['message'] = message
-            response = requests.post(url, headers=headers, data=data, auth=auth)
+    if options and send_image and picture_path:
+        data['caption'] = formatted_message
+        data['compress'] = "True"
+        files = {'image': ('image', open(options['picture_path'], 'rb'), 'image/png')}
+    else:
+        data['message'] = formatted_message
+        files = None
 
+    try:
+        response = requests.post(url, headers=headers, data=data, auth=auth, files=files)
         response.raise_for_status()
-        return response
-    except requests.RequestException as e:
-        logging.error(f"Error sending WhatsApp message: {e}")
+        logging.info(f"Message sent successfully: {message}")
+    except requests.exceptions.RequestException as e:
+        logging.error(f"An error occurred: {e}")
         return None
 
+    return response
 
 if __name__ == "__main__":
-    message = "Test message"
-    send_image = False
-    print(send_whatsapp_message(message, send_image))
-    if send_image:
-        picture_path = "path/to/your/image.png"
-        print(send_whatsapp_message(message, send_image, picture_path))
+    message = {
+        "description": "This is a test message from JellyHookAPI.",
+    }
+    options = {"send_image": True, "picture_path": "https://picsum.photos/200"}
+    response = send_message(message, options)
+    if response:
+        logging.info(f"Response status code: {response.status_code}")
+        logging.info(f"Response content: {response.content}")
 
