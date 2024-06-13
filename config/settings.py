d0a515b2d7a458c86c79e28eb0e7f8ae216fbfb5
Garnajee <>
2024-06-13 16:48:37 +0200

    fix: reading right env file now

--- config/settings.py
+++ config/settings.py
@@ -3,7 +3,8 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+dotenv_path = os.path.join(os.path.dirname(__file__), '..', '.env')
+load_dotenv(dotenv_path)
 
 TMDB_API_KEY = os.getenv("TMDB_API_KEY")
 
