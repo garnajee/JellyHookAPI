0986cd114b26372bc85e82e1f9cfe168ed8efa3e
garnajee <62147746+garnajee@users.noreply.github.com>
2025-08-15 00:06:02 +0200

    add formatted tech details to messages

---
+++ connectors/discord/discord_service.py
@@ -0,0 +1,134 @@
+#!/usr/bin/env python3
+
+import os
+import json
+import requests
+from dotenv import load_dotenv
+from datetime import datetime
+import logging
+
+load_dotenv()
+
+DISCORD_WEBHOOK_URL = os.getenv("DISCORD_WEBHOOK_URL")
+
+def format_message_for_discord(message: dict, options: dict) -> dict:
+    """
+    Format the message for Discord with improved technical details layout.
+
+    Args:
+        message (dict): Message to send
+        options (dict, optional): Additional options for the message
+
+    Returns:
+        dict: The formatted payload for Discord.
+    """
+    trailers = message.get("trailer", [])
+    if len(trailers) == 1:
+        trailer_text = f"\n[Trailer]({trailers[0]})"
+    elif len(trailers) == 2:
+        trailer_text = f"\n[Trailer FR]({trailers[0]})\n[Trailer EN]({trailers[1]})"
+    else:
+        trailer_text = ""
+
+    links = message.get("media_link", {})
+    link_text = ""
+    if "imdb" in links:
+        link_text += f"\n[IMDb]({links['imdb']})"
+    if "tmdb" in links:
+        link_text += f"\n[TMDb]({links['tmdb']})"
+
+    embed = {
+        "title": message.get("title", "Notification"),
+        "description": message.get("description","") + "\n" + link_text.strip() + "\n" + trailer_text.strip(),
+        "color": 3447003,  # light blue
+        "fields": [],
+        "footer": {
+            "text": datetime.now().strftime("%H:%M - %d/%m/%Y")
+        }
+    }
+
+    technical_details = message.get("technical_details")
+    if technical_details:
+        details_lines = []
+        
+        if 'video' in technical_details:
+            video = technical_details['video']
+            video_line = f"🎥 **Vidéo :** {video.get('resolution', 'N/A')} | {video.get('codec', 'N/A')} | {video.get('hdr', 'N/A')}"
+            details_lines.append(video_line)
+
+        if 'audio' in technical_details and technical_details['audio']:
+            audio_tracks_str = " | ".join(technical_details['audio'])
+            details_lines.append(f"🔊 **Audio :** {audio_tracks_str}")
+
+        if 'subtitles' in technical_details and technical_details['subtitles']:
+            subtitle_tracks_str = " | ".join(technical_details['subtitles'])
+            details_lines.append(f"💬 **Sous-titres :** {subtitle_tracks_str}")
+
+
+        if details_lines:
+            embed["fields"].append({
+                "name": "Détails Techniques",
+                "value": "\n".join(details_lines),
+                "inline": False
+            })
+
+    data = {
+        "content": "",
+        "embeds": [embed]
+    }
+
+    if options.get('send_image') and options.get('picture_path'):
+        data["embeds"][0]["image"] = {"url": f"attachment://{os.path.basename(options['picture_path'])}"}
+
+    return data
+
+def send_message(message: dict, options: dict = None) -> requests.Response:
+    """
+    Send message to a Discord webhook
+
+    Args:
+        message (dict): Message to send
+        options (dict, optional): Additional options for the message
+
+    Returns:
+        requests.Response: Response from the Discord API.
+    """
+    if not DISCORD_WEBHOOK_URL:
+        logging.error("DISCORD_WEBHOOK_URL is not set in the environment variables.")
+        raise ValueError("DISCORD_WEBHOOK_URL is not set in the environment variables.")
+
+    payload = format_message_for_discord(message, options)
+
+    try:
+        files = None
+        if options.get('send_image') and options.get('picture_path'):
+            files = {
+                'payload_json': (None, json.dumps(payload), 'application/json'),
+                'file1': (open(options['picture_path'], 'rb'))
+            }
+            response = requests.post(DISCORD_WEBHOOK_URL, files=files)
+        else:
+            response = requests.post(DISCORD_WEBHOOK_URL, json=payload)
+
+        response.raise_for_status()
+        logging.info(f"Message sent successfully: {message}")
+    except requests.exceptions.RequestException as e:
+        logging.error(f"An error occurred: {e}")
+        logging.error(f"Response content: {response.content if response else 'No response'}")
+        return None
+
+    return response
+
+if __name__ == "__main__":
+    message = {
+        "title": "Sample Title",
+        "description": "This is a test description.",
+        "media_link": "https://example.com",
+        "trailer": "https://youtube.com/trailer"
+    }
+    options = {"send_image": True, "picture_path": "https://picsum.photos/200"}
+    response = send_message(message, options)
+    if response:
+        logging.info(f"Response status code: {response.status_code}")
+        logging.info(f"Response content: {response.content}")
+
---
+++ connectors/matrix/matrix_service.py
@@ -0,0 +1,226 @@
+#!/usr/bin/env python3
+
+import os
+import requests
+from dotenv import load_dotenv
+import logging
+
+#logging.basicConfig(level=logging.DEBUG,format='%(asctime)s - %(levelname)s - %(filename)s - %(funcName)s - %(message)s')
+
+load_dotenv()
+
+MATRIX_URL = os.getenv("MATRIX_URL")
+ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
+ROOM_ID = os.getenv("ROOM_ID")
+
+def format_message(message: dict) -> str:
+    """
+    Format message for Matrix channel.
+
+    Args:
+        message (dict): Message to send.
+
+    Returns:
+        str: Formatted message for Matrix.
+    """
+
+    try:
+        message_parts = []
+
+        if message.get('title'):
+            message_parts.append(f"#{message.get('title')}")
+
+        technical_details = message.get("technical_details")
+        if technical_details:
+            tech_info_parts = []
+            
+            if 'video' in technical_details:
+                video = technical_details['video']
+                video_str = f"🎥 {video.get('resolution', '')} | {video.get('codec', '')} | {video.get('hdr', '')}"
+                tech_info_parts.append(video_str)
+
+            if 'audio' in technical_details and technical_details['audio']:
+                audio_str = f"🔊 {' | '.join(technical_details['audio'])}"
+                tech_info_parts.append(audio_str)
+                
+            if 'subtitles' in technical_details and technical_details['subtitles']:
+                subs_str = f"💬 {' | '.join(technical_details['subtitles'])}"
+                tech_info_parts.append(subs_str)
+            
+            if tech_info_parts:
+                message_parts.append(f"\n> {'  •  '.join(tech_info_parts)}")
+
+        if message.get('description'):
+            message_parts.append(f"\n```{message.get('description')}```")
+
+        links_section = []
+        links = message.get("media_link", {})
+        if "imdb" in links:
+            links_section.append(f"[IMDb]({links['imdb']})")
+        if "tmdb" in links:
+            links_section.append(f"[TMDb]({links['tmdb']})")
+
+        trailers = message.get("trailer", [])
+        if len(trailers) == 1:
+            links_section.append(f"[Trailer]({trailers[0]})")
+        elif len(trailers) == 2:
+            links_section.append(f"[Trailer FR]({trailers[0]})")
+            links_section.append(f"[Trailer EN]({trailers[1]})")
+        
+        if links_section:
+            message_parts.append("\n" + "\n".join(links_section))
+
+        return "\n".join(message_parts)
+
+    except Exception as e:
+        logging.error(f"Error formatting message for Matrix: {e}")
+        return ""
+
+def html_format_message(message: dict) -> str:
+    """
+    Format message for Matrix channel in HTML.
+
+    Args:
+        message (dict): Message to send.
+
+    Returns:
+        str: Formatted message for Matrix in HTML.
+    """
+    try:
+        trailers = message.get("trailer", [])
+        if len(trailers) == 1:
+            trailer_text = f'<br><a href="{trailers[0]}">Trailer</a>'
+        elif len(trailers) == 2:
+            trailer_text = f'<br><a href="{trailers[0]}">Trailer FR</a><br><a href="{trailers[1]}">Trailer EN</a>'
+        else:
+            trailer_text = ""
+
+        links = message.get("media_link", {})
+        link_text = ""
+        if "imdb" in links:
+            link_text += f'<br><br><a href="{links["imdb"]}">IMDb</a>'
+        if "tmdb" in links:
+            link_text += f'<br><a href="{links["tmdb"]}">TMDb</a>'
+
+        formatted_message = f'<h1>{message.get("title")}</h1><br/>'
+        if message.get('description'):
+            formatted_message += f'<pre>{message.get("description")}</pre>'
+        formatted_message += link_text
+        formatted_message += trailer_text
+        return formatted_message
+    except Exception as e:
+        logging.error(f"Error formatting message: {e}")
+        return ""
+
+def upload_image(image_path: str) -> str:
+    """
+    Upload image to the Matrix server.
+
+    Args:
+        image_path (str): The local path to the image.
+
+    Returns:
+        str: The content URI of the uploaded image.
+    """
+    try:
+        url = f"{MATRIX_URL}/_matrix/media/r0/upload?filename={os.path.basename(image_path)}"
+
+        headers = {
+            "Authorization": f"Bearer {ACCESS_TOKEN}",
+            "Content-Type": "image/jpeg"
+        }
+        with open(image_path, 'rb') as image_file:
+            response = requests.post(url, headers=headers, data=image_file)
+        return response.json().get("content_uri")
+    except requests.exceptions.RequestException as e:
+        logging.error(f"Error uploading image: {e}")
+        return ""
+
+def send_message(message: dict, options: dict = None) -> requests.Response:
+    """
+    Send a message to a Matrix room.
+
+    Args:
+        message (dict): The message to send.
+        options (dict): Additional options for the message, including image path.
+
+    Returns:
+        Response object: The response from the Matrix server.
+    """
+    headers = {
+        "Authorization": f"Bearer {ACCESS_TOKEN}",
+        "Content-Type": "application/json"
+    }
+    try:
+        formatted_message = format_message(message)
+        html_formatted_message = html_format_message(message)
+
+        # Check if we need to send the image
+        send_image = options.get('send_image')
+        image_path = options.get('picture_path')
+        if send_image:
+            if not image_path:
+                logging.error("image_path is None")
+                raise ValueError("image_path is None")
+        
+            # Upload the image and get the content URI
+            image_uri = upload_image(image_path)
+            if not image_uri:
+                logging.error("Failed to upload image")
+                raise Exception("Failed to upload image")
+
+            # Send the image message
+            send_image_url = f"{MATRIX_URL}/_matrix/client/r0/rooms/{ROOM_ID}/send/m.room.message"
+            image_info = {
+                "msgtype": "m.image",
+                "body": os.path.basename(image_path),
+                "url": image_uri,
+                "info": {
+                    "mimetype": "image/jpeg",
+                    "size": os.path.getsize(image_path),
+                    "w": 342,
+                    "h": 513
+                }
+            }
+
+            response = requests.post(send_image_url, headers=headers, json=image_info)
+            response.raise_for_status()
+
+        # Send the formatted message
+        send_text_url = f"{MATRIX_URL}/_matrix/client/r0/rooms/{ROOM_ID}/send/m.room.message"
+        # associated documentation: https://spec.matrix.org/latest/client-server-api/#send-m-room-message
+        # changed from m.notice to m.text and comment format and formatted_body as I encounter issue sending message.
+        text_info = {
+            "msgtype": "m.text", # m.notice: for automated client, no answer expected
+            "body": formatted_message, # notice text to send. Plain text, if html client formatting is not supported
+            #"format": "org.matrix.custom.html", # format used in formatted_body
+            #"formatted_body": html_formatted_message # formatted version of body. Required if format is specified
+        }
+        response = requests.post(send_text_url, headers=headers, json=text_info)
+        response.raise_for_status()
+
+        return response
+    except requests.exceptions.RequestException as e:
+        logging.error(f"Error sending message: {e}")
+        return None
+
+if __name__ == "__main__":
+    message = {
+        "title": "Example Title",
+        "description": "Example Description",
+        "trailer": ["http://example.com/trailer1", "http://example.com/trailer2"],
+        "media_link": {
+            "imdb": "http://example.com/imdb",
+            "tmdb": "http://example.com/tmdb"
+        }
+    }
+    options = {
+        "send_image": True,
+        "picture_path": "https://image.tmdb.org/t/p/w342/t1i10ptOivG4hV7erkX3tmKpiqm.jpg"
+    }
+    response = send_message(message, options)
+    if response:
+        logging.info("Response:", response.status_code)
+    else:
+        logging.info("Failed to send message")
+
